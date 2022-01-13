0e9230e4a0699e1decad57d6c2cb5e462d486e7b
iftakhar ahmed <ifat802@gmail.com>
2022-01-13 17:24:28 +0600

    🐛 Get a book

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ func main() {
 
 	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
 	router.HandleFunc("/books", handlers.AddBooks).Methods(http.MethodPost)
+	router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
 
 	log.Println("API is running!")
 	http.ListenAndServe(":4000", router)
